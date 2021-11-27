6b0aeabe83ac151ac6b1dee5933209c04417b20b
Peroy Nel <peroynel@yahoo.com>
2021-11-27 16:45:27 +0200

    initial commit

---
+++ src/components/Results.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Results = () => {
+    return (
+        <div>
+            Results
+            
+        </div>
+    );
+}
---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,29 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const ResultContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const ResultContextProvider = ({ children }) => {
+    const [results, setResults] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const getResults = async(type) => {
+        setIsLoading(true);
+        const response = await fetch(`${baseUrl}${type}`, {
+            method: 'GET',
+            headers: {
+                'x-user-agent': 'desktop',
+                'x-proxy-location': 'US',
+                'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+                'x-rapidapi-key': '7275795ca0msh883b96b5fc78302p1c58c4jsn8124510bcd8a'
+            }
+        });
+        const data = await response.json();
+
+        setResults(data);
+        setIsLoading(false);
+
+    }
+
+}
