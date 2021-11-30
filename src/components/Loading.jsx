454453812a405269ff1f1404b6bf24d97339007e
Peroy Nel <peroynel@yahoo.com>
2021-11-30 15:15:56 +0200

    navbar complete, search to follow
    Further component updates

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import Loader from 'react-loader-spinner';
 
-export const Loading = () => {
-    return (
-        <div className="flex jusitfy-center item-center">
-            <Loader type="Puff" color="#00BFFF" height={550} width={80}/>
-            
-        </div>
-    )
-}
+export const Loading = () => (
+  <div className="flex justify-center items-center ">
+    <Loader type="Puff" color="#00BFFF" height={550} width={80} />
+  </div>
+);
--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -6,7 +6,7 @@ const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
 export const ResultContextProvider = ({ children }) => {
     const [results, setResults] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('');
+    const [searchTerm, setSearchTerm] = useState('React');
 
     const getResults = async(type) => {
         setIsLoading(true);
@@ -21,6 +21,14 @@ export const ResultContextProvider = ({ children }) => {
         });
         
         const data = await response.json();
+
+        if(type.includes('/news')) {
+            setResults(data.entries);
+        } else if(type.includes('/images')) {
+            setResults(data.image_results);
+        } else {
+            setResults(data.results);
+        }
 
         console.log(data);
 
